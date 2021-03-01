27fe4fc923033134e6d2ec55fe14c6cbde02f64b
suhpark <laztwo@hanmail.net>
2021-03-01 14:37:37 +0900

    투두 추가하기

--- lib/api/todo.ts
+++ lib/api/todo.ts
@@ -3,3 +3,14 @@ import { TodoType } from "../../types/todo";
 
 export const getTodosApi = () => axios.get<TodoType[]>("api/todos");
 export const checkTodoAPI = (id: number) => axios.patch(`api/todos/${id}`);
+
+// 투두 추가하기 API Body
+interface AddTodoAPIBody {
+    text: string;
+    color: TodoType["color"]
+}
+
+// 투두 추가하기 API
+export const addTodoAPI = (body: AddTodoAPIBody) => {
+    axios.post("/api/todos", body);
+}
