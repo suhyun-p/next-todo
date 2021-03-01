b47d856457fe7253c9d0a1339723ca998ef80ce9
suhpark <laztwo@hanmail.net>
2021-03-01 17:20:19 +0900

    7.3 리덕스 사용하기

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import TodoList from "../components/TodoList";
 import { TodoType } from "../types/todo";
 import Axios from "axios";
 import { getTodosApi } from "../lib/api/todo";
+import {wrapper} from "../store";
+import { todoActions } from "../store/todo";
 
 interface IProps {
     todos: TodoType[];
@@ -13,14 +15,18 @@ const app: NextPage<IProps> = ({todos}) => {
     return <TodoList todos={todos} />
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-    try {
-        const { data } = await getTodosApi();
-        return { props: { todos: data} };
-    } catch (e) {
-        console.log(e);
-        return { props: { todos: []} }
+export const getServerSideProps = wrapper.getServerSideProps(
+    async ({store}) => {
+        console.log(store);
+        try {
+            const { data } = await getTodosApi();
+            store.dispatch(todoActions.setTodo(data));
+            return { props: { todos: data} };
+        } catch (e) {
+            console.log(e);
+            return { props: { todos: []} }
+        }
     }
-}
+)
 
 export default app;
--- store/todo.ts
+++ store/todo.ts
@@ -3,12 +3,13 @@ import {TodoType} from "../types/todo";
 // 3. 항상 npm-module-or-app/reducer/ACTION_TYPE 형태의 action 타입을 가져야 합니다.
 // 액션타입 정의
 export const INIT_TODO_LIST = "todo/INIT_TODO_LIST";
+export const SET_TODO_LIST = "todo/INIT_TODO_LIST";
 
 // 2. 항상 모듈의 action 생성자들을 함수형태로 export 해야 합니다.
 // 액션 생성자 정의
 export const setTodo = (payload: TodoType[]) => {
     return {
-        type: INIT_TODO_LIST,
+        type: SET_TODO_LIST,
         payload
     };
 };
@@ -28,7 +29,7 @@ const initialState: TodoReduxState = {
 // 리듀서
 export default function reducer(state = initialState, action: any) {
     switch (action.type) {
-        case INIT_TODO_LIST:
+        case SET_TODO_LIST:
             const newState = { ...state, todos: action.payload};
             return newState;
         default:
