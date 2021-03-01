3fdd718b0cbd54bf9d63c45a10177a65b8c9935d
suhpark <laztwo@hanmail.net>
2021-03-01 17:32:41 +0900

    7.4 리덕스 툴킷

--- store/index.ts
+++ store/index.ts
@@ -1,17 +1,18 @@
-import {createStore, applyMiddleware, combineReducers} from "redux";
 import {HYDRATE, createWrapper} from "next-redux-wrapper";
 import todo from "./todo";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
-    todo,
+    todo: todo.reducer,
 });
 
 const reducer = (state, action) => {
     if (action.type === HYDRATE) {
         const nextState = {
-            ...state,
-            ...action.payload,
+            ...state, // use previous state
+            ...action.payload, // apply delta from hydration
         };
+        if(state.count) nextState.count = state.count;
         return nextState;
     }
     return rootReducer(state, action);
@@ -20,17 +21,11 @@ const reducer = (state, action) => {
 // 스토어의 타입
 export type RootState = ReturnType<typeof rootReducer>;
 
-// 미들웨어 적용을 위한 스토어 enhancer
-const bindMiddleware = (middleware: any) => {
-    if(process.env.NODE_ENV !== "production") {
-        const {composeWithDevTools} = require("redux-devtools-extension");
-        return composeWithDevTools(applyMiddleware(...middleware));
-    }
-    return applyMiddleware(...middleware);
-};
-
 const initStore = () => {
-    return createStore(reducer, bindMiddleware([]));
+    return configureStore({
+        reducer,
+        devTools: true,
+    })
 };
 
 export const wrapper = createWrapper(initStore);
--- store/todo.ts
+++ store/todo.ts
@@ -1,38 +1,26 @@
-import {TodoType} from "../types/todo";
-
-// 3. 항상 npm-module-or-app/reducer/ACTION_TYPE 형태의 action 타입을 가져야 합니다.
-// 액션타입 정의
-export const INIT_TODO_LIST = "todo/INIT_TODO_LIST";
-export const SET_TODO_LIST = "todo/INIT_TODO_LIST";
-
-// 2. 항상 모듈의 action 생성자들을 함수형태로 export 해야 합니다.
-// 액션 생성자 정의
-export const setTodo = (payload: TodoType[]) => {
-    return {
-        type: SET_TODO_LIST,
-        payload
-    };
-};
-
-export const todoActions = {setTodo};
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { TodoType } from "../types/todo";
 
 interface TodoReduxState {
-    todos: TodoType[];
+  todos: TodoType[];
 }
 
-// 초기 상태
+//* 초기 상태
 const initialState: TodoReduxState = {
-    todos: []
-}
+  todos: [],
+};
 
-// 1. 항상 reducer() 란 이름의 함수를 export default 해야 합니다.
-// 리듀서
-export default function reducer(state = initialState, action: any) {
-    switch (action.type) {
-        case SET_TODO_LIST:
-            const newState = { ...state, todos: action.payload};
-            return newState;
-        default:
-            return state;
-    }
-}
+const todo = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    //* 투두 변경하기
+    setTodo(state, action: PayloadAction<TodoType[]>) {
+      state.todos = action.payload;
+    },
+  },
+});
+
+export const todoActions = { ...todo.actions };
+
+export default todo;
